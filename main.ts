c3b2119eb13e31ced25c2560b0491557ba63d78b
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2024-01-15 13:28:35 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ihor Chernyshev
+ * Created on: Jan 2024
+ * This program will allow you to control Micro:Bit from your phone via Bluetooth
 */
 
-basic.showString('Hello, World!')
+basic.showIcon(IconNames.Happy)
+
+devices.onGamepadButton(MesDpadButtonInfo.ADown, function () {
+  robotbit.MotorRun(robotbit.Motors.M1A, 200)
+  robotbit.MotorRun(robotbit.Motors.M2A, 200)
+})
+
+devices.onGamepadButton(MesDpadButtonInfo.BDown, function () {
+  robotbit.MotorRun(robotbit.Motors.M1A, -200)
+  robotbit.MotorRun(robotbit.Motors.M2A, -200)
+})
