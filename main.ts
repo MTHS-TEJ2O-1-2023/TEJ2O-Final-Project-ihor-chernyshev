a9b45c36e9dd51fb3e85091c1667390dfee8d9f0
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2024-01-16 12:26:59 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -4,14 +4,27 @@
  * This program will allow you to control Micro:Bit from your phone via Bluetooth
 */
 
+// variables
+let test1 = 0
+
+
+// setup
 basic.showIcon(IconNames.Happy)
 
-devices.onGamepadButton(MesDpadButtonInfo.ADown, function () {
+// go straight
+devices.onGamepadButton(MesDpadButtonInfo.AUp, function () {
   robotbit.MotorRun(robotbit.Motors.M1A, 200)
   robotbit.MotorRun(robotbit.Motors.M2A, 200)
 })
 
-devices.onGamepadButton(MesDpadButtonInfo.BDown, function () {
+// go back
+devices.onGamepadButton(MesDpadButtonInfo.BUp, function () {
   robotbit.MotorRun(robotbit.Motors.M1A, -200)
   robotbit.MotorRun(robotbit.Motors.M2A, -200)
+})
+
+// stop
+devices.onGamepadButton(MesDpadButtonInfo._1Up, function () {
+  robotbit.MotorRun(robotbit.Motors.M1A, 0)
+  robotbit.MotorRun(robotbit.Motors.M2A, 0)
 })
