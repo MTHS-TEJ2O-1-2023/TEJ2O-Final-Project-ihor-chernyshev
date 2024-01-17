3b53d1643ac69cd54369905e91273889f66de4e0
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2024-01-17 13:27:22 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -4,27 +4,57 @@
  * This program will allow you to control Micro:Bit from your phone via Bluetooth
 */
 
-// variables
-let test1 = 0
-
-
 // setup
 basic.showIcon(IconNames.Happy)
 
+// says that bluetooh is connceted
+bluetooth.onBluetoothConnected(function () {
+  pins.digitalWritePin(DigitalPin.P2, 1)
+  basic.pause(300)
+  pins.digitalWritePin(DigitalPin.P2, 0)
+})
+
 // go straight
 devices.onGamepadButton(MesDpadButtonInfo.AUp, function () {
-  robotbit.MotorRun(robotbit.Motors.M1A, 200)
-  robotbit.MotorRun(robotbit.Motors.M2A, 200)
+  robotbit.MotorRun(robotbit.Motors.M1A, 250)
+  robotbit.MotorRun(robotbit.Motors.M2A, 250)
 })
 
 // go back
 devices.onGamepadButton(MesDpadButtonInfo.BUp, function () {
-  robotbit.MotorRun(robotbit.Motors.M1A, -200)
-  robotbit.MotorRun(robotbit.Motors.M2A, -200)
+  robotbit.MotorRun(robotbit.Motors.M1A, -250)
+  robotbit.MotorRun(robotbit.Motors.M2A, -250)
+})
+
+// turn left
+devices.onGamepadButton(MesDpadButtonInfo.CUp, function () {
+  robotbit.MotorRun(robotbit.Motors.M1A, 250)
+  robotbit.MotorRun(robotbit.Motors.M2A, 50)
+})
+
+// turn right
+devices.onGamepadButton(MesDpadButtonInfo.DUp, function () {
+  robotbit.MotorRun(robotbit.Motors.M1A, 50)
+  robotbit.MotorRun(robotbit.Motors.M2A, 250)
 })
 
 // stop
 devices.onGamepadButton(MesDpadButtonInfo._1Up, function () {
-  robotbit.MotorRun(robotbit.Motors.M1A, 0)
-  robotbit.MotorRun(robotbit.Motors.M2A, 0)
+  robotbit.MotorStopAll()
+})
+
+// turn on lights
+devices.onGamepadButton(MesDpadButtonInfo._3Up, function() {
+  pins.digitalWritePin(DigitalPin.P1, 1)
+})
+
+// turn off lights
+devices.onGamepadButton(MesDpadButtonInfo._4Up, function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+})
+
+// if bluetooth is disconnected
+bluetooth.onBluetoothDisconnected(function () {
+  robotbit.MotorStopAll()
+  pins.digitalWritePin(DigitalPin.P2, 1)
 })
