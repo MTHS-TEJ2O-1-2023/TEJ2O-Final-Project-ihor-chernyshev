04e373f0d7c13b8ebd3b39d732879863e759078c
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2024-01-22 07:25:54 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -7,54 +7,31 @@
 // setup
 basic.showIcon(IconNames.Happy)
 
-// says that bluetooh is connceted
-bluetooth.onBluetoothConnected(function () {
-  pins.digitalWritePin(DigitalPin.P2, 1)
-  basic.pause(300)
-  pins.digitalWritePin(DigitalPin.P2, 0)
-})
-
 // go straight
 devices.onGamepadButton(MesDpadButtonInfo.AUp, function () {
-  robotbit.MotorRun(robotbit.Motors.M1A, 250)
-  robotbit.MotorRun(robotbit.Motors.M2A, 250)
+  robotbit.MotorRun(robotbit.Motors.M1A, 255)
+  robotbit.MotorRun(robotbit.Motors.M2A, 255)
 })
 
 // go back
 devices.onGamepadButton(MesDpadButtonInfo.BUp, function () {
-  robotbit.MotorRun(robotbit.Motors.M1A, -250)
-  robotbit.MotorRun(robotbit.Motors.M2A, -250)
+  robotbit.MotorRun(robotbit.Motors.M1A, -254)
+  robotbit.MotorRun(robotbit.Motors.M2A, -254)
 })
 
 // turn left
 devices.onGamepadButton(MesDpadButtonInfo.CUp, function () {
-  robotbit.MotorRun(robotbit.Motors.M1A, 250)
-  robotbit.MotorRun(robotbit.Motors.M2A, 50)
+  robotbit.MotorRun(robotbit.Motors.M1A, 255)
+  robotbit.MotorRun(robotbit.Motors.M2A, 80)
 })
 
 // turn right
 devices.onGamepadButton(MesDpadButtonInfo.DUp, function () {
-  robotbit.MotorRun(robotbit.Motors.M1A, 50)
-  robotbit.MotorRun(robotbit.Motors.M2A, 250)
+  robotbit.MotorRun(robotbit.Motors.M1A, 80)
+  robotbit.MotorRun(robotbit.Motors.M2A, 255)
 })
 
 // stop
 devices.onGamepadButton(MesDpadButtonInfo._1Up, function () {
   robotbit.MotorStopAll()
-})
-
-// turn on lights
-devices.onGamepadButton(MesDpadButtonInfo._3Up, function () {
-  pins.digitalWritePin(DigitalPin.P1, 1)
-})
-
-// turn off lights
-devices.onGamepadButton(MesDpadButtonInfo._4Up, function () {
-  pins.digitalWritePin(DigitalPin.P1, 0)
-})
-
-// if bluetooth is disconnected
-bluetooth.onBluetoothDisconnected(function () {
-  robotbit.MotorStopAll()
-  pins.digitalWritePin(DigitalPin.P2, 1)
 })
