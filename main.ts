a725296974ad2d5ed827159ee461306e10d14210
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2024-01-22 21:17:45 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -22,12 +22,12 @@ devices.onGamepadButton(MesDpadButtonInfo.BUp, function () {
 // turn left
 devices.onGamepadButton(MesDpadButtonInfo.CUp, function () {
   robotbit.MotorRun(robotbit.Motors.M1A, 255)
-  robotbit.MotorRun(robotbit.Motors.M2A, -255)
+  robotbit.MotorRun(robotbit.Motors.M2A, -254)
 })
 
 // turn right
 devices.onGamepadButton(MesDpadButtonInfo.DUp, function () {
-  robotbit.MotorRun(robotbit.Motors.M1A, -255)
+  robotbit.MotorRun(robotbit.Motors.M1A, -254)
   robotbit.MotorRun(robotbit.Motors.M2A, 255)
 })
 
